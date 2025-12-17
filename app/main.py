e311c3b3e6024c236fb2e1fa4a0d6cd1b88cf78c
luvremak <luvremak@gmail.com>
2025-12-18 00:32:37 +0200

    feat: alembic

--- app/main.py
+++ app/main.py
@@ -1,17 +1,10 @@
 import asyncio
 
-from sqlalchemy.ext.asyncio import create_async_engine
-
-from app.core.database import database, metadata
-from app.core.settings import settings
+from app.core.database import database
 from app.tg_bot.tg_bot import dp, bot
 
 
 async def main():
-    engine = create_async_engine(str(settings.DB_URI))
-
-    async with engine.begin() as conn:
-        await conn.run_sync(metadata.create_all)
     await database.connect()
     await dp.start_polling(bot)
 
